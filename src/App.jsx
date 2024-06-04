1e6e355f05609709161bf7e0ec22c4ae3b4b6bb1
Preslav977 <p.cvetanow@gmail.com>
2024-06-04 14:17:12 +0300

    Created two contexts which are going to be used to fetch posts and see if user is logged in.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import "./index.css";
+import { useState, createContext } from "react";
+import { Outlet } from "react-router-dom";
+
+export const PostContext = createContext(null);
+
+export const IsUserLoggedInContext = createContext(null);
 
 function App() {
-  return <></>;
+  const [posts, setPosts] = useState([]);
+
+  const [IsUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+  return (
+    <>
+      <PostContext.Provider value={[posts, setPosts]}>
+        <IsUserLoggedInContext.Provider
+          value={[IsUserLoggedIn, setIsUserLoggedIn]}
+        ></IsUserLoggedInContext.Provider>
+      </PostContext.Provider>
+    </>
+  );
 }
 
 export default App;
