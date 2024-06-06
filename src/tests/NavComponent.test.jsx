b7be69a4bf253a73772aa4f608e0e0a8e41e51bf
Preslav977 <p.cvetanow@gmail.com>
2024-06-06 07:24:37 +0300

    Created test for rendering the navigation links in NavComponent.

--- src/tests/NavComponent.test.jsx
+++ src/tests/NavComponent.test.jsx
@@ -28,4 +28,31 @@ describe("Should render NavComponent", () => {
 
     expect(screen.queryByRole("button")).not.toBeInTheDocument();
   });
+
+  it("should render NavComponent navigation links", async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/home"],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const user = userEvent.setup();
+
+    const readLink = screen.getByTestId("read");
+
+    await user.click(readLink);
+
+    expect(screen.queryByText("Topics").textContent).toMatch(/topics/i);
+
+    expect(screen.queryByText("Folklore").textContent).toMatch(/folklore/i);
+    expect(screen.queryByText("Folklore Music").textContent).toMatch(
+      /Folklore Music/i,
+    );
+
+    expect(screen.queryByText("Culture").textContent).toMatch(/culture/i);
+    expect(screen.queryByText("History").textContent).toMatch(/history/i);
+    expect(screen.queryByText("Nature").textContent).toMatch(/nature/i);
+    expect(screen.queryByText("Traditions").textContent).toMatch(/traditions/i);
+    expect(screen.queryByText("Customs").textContent).toMatch(/customs/i);
+  });
 });
