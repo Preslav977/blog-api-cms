24b009f5d371534a57e30f48d0be1acbb8642098
Preslav977 <p.cvetanow@gmail.com>
2024-06-07 09:49:15 +0300

    Created test that expects posts to be render based on history category in FetchPostsByCategory

--- src/tests/FetchPostsByCategory.test.jsx
+++ src/tests/FetchPostsByCategory.test.jsx
@@ -75,4 +75,64 @@ describe("should render FlexedComponent posts by category", () => {
 
     expect(postImg).toBeInTheDocument();
   });
+
+  it("should render history posts category on the main page", async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: [
+        "/home",
+        "/home/posts/category/6644691ff1f4a04823a2c01e",
+      ],
+      initialIndex: 1,
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const apiLoading = screen.queryByTestId("loading");
+
+    expect(apiLoading).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() => screen.queryByTestId("loading"));
+
+    const postImg = screen.getAllByTestId("postImg");
+
+    const postCategory = screen.queryAllByText("history");
+
+    const postTitle = screen.queryByText("Pleven Epopee 1877 Panorama, Pleven");
+
+    const postTitleOne = screen.queryByText("Tsar Simeon The Great");
+
+    const postTitleTwo = screen.queryByText(
+      "Vasil Levski - unsurpassed in courage, defied an empire",
+    );
+
+    const postBody = screen.getAllByTestId("postBody");
+
+    expect(postCategory[0].textContent).toEqual("history");
+
+    expect(postCategory[1].textContent).toEqual("history");
+
+    expect(postCategory[2].textContent).toEqual("history");
+
+    expect(postTitle.textContent).toEqual(
+      "Pleven Epopee 1877 Panorama, Pleven",
+    );
+
+    expect(postTitleOne.textContent).toEqual("Tsar Simeon The Great");
+
+    expect(postTitleTwo.textContent).toEqual(
+      "Vasil Levski - unsurpassed in courage, defied an empire",
+    );
+
+    expect(postBody[0]).toBeInTheDocument();
+
+    expect(postBody[1]).toBeInTheDocument();
+
+    expect(postBody[2]).toBeInTheDocument();
+
+    expect(postImg[0]).toBeInTheDocument();
+
+    expect(postImg[1]).toBeInTheDocument();
+
+    expect(postImg[2]).toBeInTheDocument();
+  });
 });
