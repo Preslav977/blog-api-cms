bd6192e711ecf6c5ecfef08bbb0f833240249409
Preslav977 <p.cvetanow@gmail.com>
2024-06-07 09:42:13 +0300

    Created test that expects the second post to be fetched in FetchPosts.

--- src/tests/FetchPosts.test.jsx
+++ src/tests/FetchPosts.test.jsx
@@ -64,4 +64,57 @@ describe("Should render FetchPosts component", () => {
 
     expect(screen.queryByText("#bulgaria").textContent).toMatch(/#bulgaria/i);
   });
+
+  it("should render second post on the main page", async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/home", "/home/posts/66446886f1f4a04823a2bff3"],
+      initialIndex: 1,
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const apiLoading = screen.queryByTestId("loading");
+
+    expect(apiLoading).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() => screen.queryByTestId("loading"));
+
+    screen.debug();
+
+    const postImg = screen.getByTestId("postImg");
+
+    const postCategory = screen.queryByText("folklore music");
+
+    const postTitle = screen.queryByText("Bulgarian Music Folklore");
+
+    const postBody = screen.getByTestId("postBody");
+
+    expect(postCategory.textContent).toEqual("folklore music");
+
+    expect(postTitle.textContent).toEqual("Bulgarian Music Folklore");
+
+    expect(postBody).toBeInTheDocument();
+
+    expect(postImg).toBeInTheDocument();
+
+    expect(screen.queryByText("Preslaw").textContent).toMatch(/preslaw/i);
+
+    expect(screen.queryByText("Cvetanow").textContent).toMatch(/cvetanow/i);
+
+    expect(
+      screen.queryByText("Photo by Trekking Bulgaria").textContent,
+    ).toMatch(/photo by trekking bulgaria/i);
+
+    screen.debug();
+
+    expect(screen.queryByText("#bulgaria").textContent).toMatch(/#bulgaria/i);
+
+    expect(screen.queryByText("#folklore").textContent).toMatch(/#folklore/i);
+
+    expect(screen.queryByText("#music").textContent).toMatch(/#music/i);
+
+    expect(screen.queryByText("#culture").textContent).toMatch(/#culture/i);
+
+    expect(screen.queryByText("#balkan").textContent).toMatch(/#balkan/i);
+  });
 });
